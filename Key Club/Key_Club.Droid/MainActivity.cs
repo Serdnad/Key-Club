bcd22a7480b787ed43191e0f780ac1ee94059760
Andres Gutierrez <andresgutierrez535@gmail.com>
2016-05-28 19:43:46 -0400

    Add project files.

---
+++ Key Club/Key_Club.Droid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Key_Club.Droid
+{
+	[Activity (Label = "Key_Club", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+			LoadApplication (new Key_Club.App ());
+		}
+	}
+}
+
